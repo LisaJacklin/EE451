5118dd41fe8ac0e27a403582e3bf167c14a778ab
LisaJacklin <lmjacklin@alaska.edu>
2023-09-01 14:58:05 -0800

    new comments on functions

--- impseq.m
+++ impseq.m
@@ -4,8 +4,9 @@ function[x, n] = impseq(n0, n1,n2)
 % -------------------
 % [x,n} = impseq(n0,n1,n2)
 %
-n = [n1:n2]; 
-x = [(n - n0) == 0]
+n = [n1:n2]; %impuse array
+x = [(n - n0) == 0] %step array
+
 %think of x here as an assert... it has to be one or zero
 %because it is being done on an array element, it will be a row of 0s or 1s
 %dependent on each individual value.
