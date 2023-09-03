ef67210c91efb47297bcfd3fd829399dbbad594f
LisaJacklin <lmjacklin@alaska.edu>
2023-09-02 20:48:52 -0800

    function adjusted

--- Functions/impseq.m
+++ Functions/impseq.m
@@ -1,6 +1,6 @@
 function[x, n] = impseq(n0, n1,n2) 
 %note that this is an impuse sequence
-%generate x(n) = delta (n-n0), n1 <+ n <= n2
+%generate x(n) = delta (n-n0), n1 <= n <= n2
 % -------------------
 % [x,n} = impseq(n0,n1,n2)
 %
