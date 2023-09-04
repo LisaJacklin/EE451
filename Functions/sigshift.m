2c668703ce766eb81370d3a8c9bba21f326dd28f
LisaJacklin <lmjacklin@alaska.edu>
2023-09-04 12:42:12 -0800

    Fix Sigshift, sigadd

--- Functions/sigshift.m
+++ Functions/sigshift.m
@@ -4,12 +4,12 @@ function [y, n] = sigshift(x, m, k)
 % k is the shifting value
 % single signal shift
 
-if k>0
-    disp(‘Positive’);
-    n1 = n(1):n(end)+k;
-    x1 = [zeros(1,k) x];
+if k > 0
+    disp("positive")
+    n = n(1):n(end)+k;
+    y = [zeros(1,k) x];
 else
-    disp(‘Negative’);
-    n1 = n(1)+k:n(end);
-    x1 = [x zeros(1,abs(k))]; % abs is for absolute value of (k) because quantity can never be (-ve) negative %
+    disp("negative")
+    n = n(1)+k:n(end);
+    y = [x zeros(1,abs(k))]; % abs is for absolute value of (k) because quantity can never be (-ve) negative %
 end
