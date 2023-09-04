9c07717fa40831414d5e115569f8244180642df4
LisaJacklin <lmjacklin@alaska.edu>
2023-09-04 10:35:01 -0800

    Problem 3 complete

--- Functions/sigadd.m
+++ Functions/sigadd.m
@@ -9,8 +9,8 @@ function [y, n] = sigadd (x1, n1, x2, n2)
 n = [min(min(n1), min(n2)): max(max(n1), max(n2))];
 %this determines the min and max for all n
 y1 = zeros(1, length(n)); y2 = y1;
-y1(find( (n >= min(n1) & (n <= max(n2))) == 1)) = x1
-y2(find( (n >= min(n1) & (n <= max(n2))) == 1)) = x2
+ y1(find((n>=min(n1))&(n<=max(n1))==1)) = x1; 
+ y2(find((n>=min(n2))&(n<=max(n2))==1)) = x2; 
 %find here is allowing us to add x1 and x2 to a biger table that is y
 y = y1 + y2
 
