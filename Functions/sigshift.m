e4dd22c1203bc0212628a1f50e876e16c0352d44
LisaJacklin <lmjacklin@alaska.edu>
2023-09-10 16:49:03 -0800

    Revert "updates"
    This reverts commit 8de8234156d8e30b1d86c104b537f9cce439eedf.

--- Functions/sigshift.m
+++ Functions/sigshift.m
@@ -1,15 +1,7 @@
-function [y, n] = sigshift(x, m, k)
-% y(n) = x(n + k)
-% -------------------
-% k is the shifting value
-% single signal shift
+function [y,n]=sigfold(x,n)
+% y(n) = x(-n)
+% --------
+% [y,n]= sigfold(x,n)
 
-if k > 0
-    disp("positive")
-    n = n(1):n(end)+k;
-    y = [zeros(1,k) x];
-else
-    disp("negative")
-    n = n(1)+k:n(end);
-    y = [x zeros(1,abs(k))]; % abs is for absolute value of (k) because quantity can never be (-ve) negative %
-end
+n = -fliplr(n);
+y = fliplr(x);
--- Functions/stepseq.m
+++ Functions/stepseq.m
@@ -1,12 +1,11 @@
-function [x,n] = stepseq(n0,n1, n2)
-%generates x(n) = u(n-n0), n1 <= n <= n2
-%-----------------------
-% [x,n] = stepseq(n0,n1,n2)
-%
-n = [n1:n2]; % step array
-x = [(n - n0) >= 0]; %index array
+function [y,n]=sigadd(x1,n1,x2,n2)
+% y(n) = x1(n) + x2(n)
+% --------
+% y is the sum over n, which includes n1, n2
+% x1 is the sequence over n1
+% x2 is the sequence over n2
 
-%remember that for a step signal, everything is zero between n1 and n2
-%everything on the left of n0 is zero, everything on the right of n0 is one
-
-%note that semicolon means it will not print to the UI
+n = min(min(n1),min(n2)):max(max(n1),max(n2))
+y1 = zeros(1,length(n)); y2 = y1;
+y1(find( (n >= min(n1) & (n <=max(n1))) == 1) = x1
+y2(find( (n >= min(n2) & (n <=max(n2))) == 1)) = x2
