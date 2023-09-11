8de8234156d8e30b1d86c104b537f9cce439eedf
LisaJacklin <lmjacklin@alaska.edu>
2023-09-10 16:48:39 -0800

    updates

--- Functions/impseq.m
+++ Functions/impseq.m
@@ -1,7 +1,15 @@
-function [x,n]=stepseq(n0,n1,n2)
-% generates x(n) = u(n-n0), n1 <= n <= n2
-% --------
-% [x,n]=stepseq(n0,n1,n2)
+function[x, n] = impseq(n0, n1,n2) 
+%note that this is an impuse sequence
+%generate x(n) = delta (n-n0), n1 <= n <= n2
+% -------------------
+% [x,n} = impseq(n0,n1,n2)
 %
-n = [n1:n2];
-x = [(n - n0) == 0]
+n = [n1:n2]; %impuse array
+x = [(n - n0) == 0] %step array
+
+%think of x here as an assert... it has to be one or zero
+%because it is being done on an array element, it will be a row of 0s or 1s
+%dependent on each individual value.
+
+%note that in order to read this file through the help command,
+%we need to be in the same search folder to obtain it
--- Functions/sigmult.m
+++ Functions/sigmult.m
@@ -1,12 +1,18 @@
-function [y,n]=sigshift(x,m,k)
+function [y, n] = sigmult(x1, n1, x2, n2)
 % y(n) = x1(n) * x2(n)
-% --------
-% y is the multiplication over n, which includes n1, n2
+% ---------------------
+% y is the product of n including n1 and n2
 % x1 is the sequence over n1
 % x2 is the sequence over n2
 
-n = min(min(n1),min(n2)):max(max(n1),max(n2))
-y1 = zeros(1,length(n)); y2 = y1;
-y1(find( (n >= min(n1) & (n <=max(n1))) == 1)) = x1
-y2(find( (n >= min(n2) & (n <=max(n2))) == 1)) = x2
-y = y1 .* y2
+%first finding the range of the each signal
+n = [min(min(n1), min(n2)): max(max(n1), max(n2))];
+%then finding the length of each signal itself
+y1 = zeros(1, length(n)); y2 = y1;
+
+%now we need to find and multiply each
+y1(find(( n >= min(n1) & (n <= max(n2))) == 1)) = x1
+y2(find(( n >= min(n1) & (n <= max(n2))) == 1)) = x2
+
+%now to multiply these two together
+y = y1 .* y2;
