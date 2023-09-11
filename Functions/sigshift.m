d7d9d1ee118ec1a53af91017943d2bd19fc08286
LisaJacklin <lmjacklin@alaska.edu>
2023-09-10 16:49:22 -0800

    Revert "updating functions"
    This reverts commit 1ec43fc9942b09ac7306e88a1c48872174c60bee.

--- Functions/sigshift.m
+++ Functions/sigshift.m
@@ -1,7 +1,15 @@
-function [y,n]=sigfold(x,n)
-% y(n) = x(-n)
-% --------
-% [y,n]= sigfold(x,n)
+function [y, n] = sigshift(x, m, k)
+% y(n) = x(n + k)
+% -------------------
+% k is the shifting value
+% single signal shift
 
-n = -fliplr(n);
-y = fliplr(x);
+if k > 0
+    disp("positive")
+    n = n(1):n(end)+k;
+    y = [zeros(1,k) x];
+else
+    disp("negative")
+    n = n(1)+k:n(end);
+    y = [x zeros(1,abs(k))]; % abs is for absolute value of (k) because quantity can never be (-ve) negative %
+end
