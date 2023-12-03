cc941e10ac92d6afa6827f836acfc59f9ce4cf2e
Lisa Jacklin <lmjacklin@alaska.edu>
2023-12-02 23:41:24 -0900

    Homework 11 started

--- Functions/FunctionsFromClass/u_buttap.m
+++ Functions/FunctionsFromClass/u_buttap.m
@@ -1,15 +1,17 @@
-function [b, a] = u_buttap(N, Omegac);
-%Unnormalized butterworth Analog Lowpass Filter Prototype
-% ------------------------------------------------
-% [ b, a] = u_battap(N, Omegac);
-%b = numerator polynomial coefficients of Ha(s)
-%a = denominator polynomial coefficients of Ha(s)
-%N = order of the butterworth filter
-% Omegac = cutoff frequency in radians/sec
+function [b,a] = u_buttap(N,Omegac);
+% Unnormalized Butterworth Analog Lowpass Filter Prototype
+% --------------------------------------------------------
+% [b,a] = u_buttap(N,Omegac);
+% b = numerator polynomial coefficients of Ha(s)
+% a = denominator polynomial coefficients of Ha(s)
+% N = Order of the Butterworth Filter
+% Omegac = Cutoff frequency in radians/sec
 %
-[z, p, k] = buttap(N);
+[z,p,k] = buttap(N);
 p = p*Omegac;
 k = k*Omegac^N;
 B = real(poly(z));
 b = k*B; a = real(poly(p));
 end
+
+
